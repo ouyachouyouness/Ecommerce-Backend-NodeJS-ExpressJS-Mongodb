51685c14bbd7a7ae305495784c68d57a9a5a0a96
OUYACHOU Younes <ouyachouyouness@gmail.com>
2021-08-07 20:07:02 +0200

    5

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const User = require('../models/user')
+const jwj = require('jsonwebtoken')
+
 
 exports.salam = (req, res) => {
     res.send({
@@ -14,5 +16,27 @@ exports.signup = (req, res) => {
         }
 
         res.send(user)
+    })
+}
+
+exports.signin = (req, res) => {
+
+    const { email, passsword} = req.body;
+
+    User.findOne({ email}, (err, user) => {
+        if(err || !user) {
+            return res.status(400).json({
+                error: 'User not found with this email, please sign up'
+            })
+        }
+        if(!user.authenticate()){
+            return res.status(401).json({
+                error: 'Email and password dont Match !'
+            })
+        }
+        const token = jwt.sign({
+            _id: user._id
+        }, process.env.JWT_SECRET)
+        
     })
 }
